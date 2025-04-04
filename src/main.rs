48a6a96324195589e8dab0b25d828f4218e302ab
Bart Slinger <bartslinger@gmail.com>
2025-04-04 22:11:16 +0800

    use mavlink crate for udp connection

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use crate::coordinate_systems::{AngleExt, LLA};
 use crate::fixed_wing::FixedWing;
 use mavlink::common::{MavAutopilot, MavMessage, MavModeFlag, MavState, MavType, HEARTBEAT_DATA};
-use mavlink::{MAVLinkV2MessageRaw, MavHeader};
+use mavlink::{write_versioned_msg, MAVLinkV2MessageRaw, MavHeader, Message};
+use std::io::BufRead;
+use std::sync::Arc;
 
 mod coordinate_systems;
 mod fixed_wing;
@@ -16,53 +18,50 @@ async fn main() -> Result<(), anyhow::Error> {
 
     let simulator = simulator::Simulator::new();
 
-    let header = MavHeader {
+    let mut header = MavHeader {
         system_id: 1,
         component_id: 1,
         sequence: 42,
     };
-    let heartbeat_data = HEARTBEAT_DATA {
-        custom_mode: 0,
-        mavtype: MavType::MAV_TYPE_FIXED_WING,
-        autopilot: MavAutopilot::MAV_AUTOPILOT_GENERIC,
-        base_mode: MavModeFlag::empty(),
-        system_status: MavState::MAV_STATE_STANDBY,
-        mavlink_version: 0x3,
-    };
-    let mut message = MAVLinkV2MessageRaw::new();
-    message.serialize_message_data(header, &heartbeat_data);
-    let bytes = message.raw_bytes();
-    tracing::info!("Bytes: {:?}", bytes);
-    let mavlink_socket = tokio::net::UdpSocket::bind("0.0.0.0:0").await?;
+    let conn = Arc::new(
+        mavlink::connect_async::<mavlink::ardupilotmega::MavMessage>("udpout:127.0.0.1:14550")
+            .await?,
+    );
 
-    loop {
-        let x = mavlink_socket.send_to(bytes, "127.0.0.1:14550").await?;
-        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-        tracing::info!("Sent {} bytes", x);
-    }
+    let uplink = conn.clone();
+    let downlink = conn;
+    // let uplink_socket = Arc::new(tokio::net::UdpSocket::bind("0.0.0.0:0").await?);
+    // uplink_socket.connect("127.0.0.1:14550").await?;
+    // let downlink_socket = uplink_socket.clone();
 
     let (uplink_tx, uplink_rx) = tokio::sync::mpsc::channel(10);
-    let (downlink_tx, mut downlink_rx) = tokio::sync::mpsc::channel(10);
+    let (downlink_tx, mut downlink_rx) =
+        tokio::sync::mpsc::channel::<mavlink::ardupilotmega::MavMessage>(10);
 
-    let interface_task = async move {
-        let mut counter = 0;
+    let downlink_task = async move {
         loop {
-            // send something to the sim
-            let send_result = uplink_tx.send(counter).await;
-            if let Err(_) = send_result {
-                tracing::info!("Uplink channel closed");
-                break;
-            }
-            // receive something
-            let recv_result = downlink_rx.recv().await;
-            if let Some(v) = recv_result {
-                tracing::info!("Downlink: {:?}", v);
+            if let Some(message) = downlink_rx.recv().await {
+                header.sequence = header.sequence.wrapping_add(1);
+                if let Err(e) = downlink.send(&header, &message).await {
+                    tracing::error!("Failed to send downlink message: {:?}", e);
+                }
             } else {
                 tracing::info!("Downlink channel closed");
                 break;
             }
-            counter += 1;
-            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        }
+    };
+
+    let uplink_task = async move {
+        loop {
+            if let Ok((header, message)) = uplink.recv().await {
+                if let Err(e) = uplink_tx.try_send(message) {
+                    tracing::error!("Failed to forward uplink message: {:?}", e);
+                }
+            } else {
+                tracing::info!("Uplink channel closed");
+                break;
+            }
         }
     };
 
@@ -70,8 +69,11 @@ async fn main() -> Result<(), anyhow::Error> {
         v = simulator.run(downlink_tx, uplink_rx) => {
             tracing::warn!("Simulator stopped: {:?}", v);
         }
-        v = interface_task => {
-            tracing::warn!("Interface task stopped: {:?}", v);
+        v = downlink_task => {
+            tracing::warn!("Downlink task stopped: {:?}", v);
+        }
+        v = uplink_task => {
+            tracing::warn!("Uplink task stopped: {:?}", v);
         }
     }
     Ok(())
--- src/simulator.rs
+++ src/simulator.rs
@@ -1,5 +1,6 @@
 use crate::coordinate_systems::{AngleExt, LLA};
 use crate::fixed_wing::FixedWing;
+use mavlink::common::{MavAutopilot, MavModeFlag, MavState, MavType};
 
 pub struct Simulator {}
 
@@ -10,30 +11,49 @@ impl Simulator {
 
     pub async fn run(
         &self,
-        downlink_tx: tokio::sync::mpsc::Sender<u32>,
-        mut uplink_rx: tokio::sync::mpsc::Receiver<u32>,
+        downlink_tx: tokio::sync::mpsc::Sender<mavlink::ardupilotmega::MavMessage>,
+        mut uplink_rx: tokio::sync::mpsc::Receiver<mavlink::ardupilotmega::MavMessage>,
     ) -> Result<(), anyhow::Error> {
         let mut fixed_wing = FixedWing::new(LLA::new(52.0, 4.5, 100.0), 25.0.degrees());
-        let mut interval = tokio::time::interval(tokio::time::Duration::from_millis(100));
+        let mut physics_interval = tokio::time::interval(tokio::time::Duration::from_millis(100));
+        let mut heartbeat_interval = tokio::time::interval(tokio::time::Duration::from_secs(2));
+
         loop {
             // tokio::select! code can't be auto-formatted, so using this enum workaround instead
             enum Trigger {
-                Interval,
-                Uplink(Option<u32>),
+                PhysicsInterval,
+                HeartbeatInterval,
+                Uplink(Option<mavlink::ardupilotmega::MavMessage>),
             }
             let trigger = tokio::select! {
-                _ = interval.tick() => Trigger::Interval,
+                _ = physics_interval.tick() => Trigger::PhysicsInterval,
+                _ = heartbeat_interval.tick() => Trigger::HeartbeatInterval,
                 v = uplink_rx.recv() => Trigger::Uplink(v),
             };
             match trigger {
-                Trigger::Interval => {
+                Trigger::PhysicsInterval => {
                     fixed_wing.simulate(0.1);
-                    tracing::info!("Local Position: {:?}", fixed_wing.local_position());
+                }
+                Trigger::HeartbeatInterval => {
+                    let message = mavlink::ardupilotmega::MavMessage::HEARTBEAT(
+                        mavlink::ardupilotmega::HEARTBEAT_DATA {
+                            custom_mode: 0,
+                            mavtype: mavlink::ardupilotmega::MavType::MAV_TYPE_FIXED_WING,
+                            autopilot:
+                                mavlink::ardupilotmega::MavAutopilot::MAV_AUTOPILOT_ARDUPILOTMEGA,
+                            base_mode: mavlink::ardupilotmega::MavModeFlag::empty(),
+                            system_status: mavlink::ardupilotmega::MavState::MAV_STATE_STANDBY,
+                            mavlink_version: 0x3,
+                        },
+                    );
+                    let send_result = downlink_tx.try_send(message);
+                    if send_result.is_err() {
+                        anyhow::bail!("Downlink channel closed");
+                    }
                 }
                 Trigger::Uplink(v) => {
                     if let Some(v) = v {
                         tracing::info!("Uplink: {:?}", v);
-                        let send_result = downlink_tx.try_send(v);
                     } else {
                         tracing::info!("Uplink channel closed");
                         anyhow::bail!("Uplink channel closed");
